f7b45e7a45ce369e7354e82964a53913425bbc10
SSOURABH58 <ssourabh025@gamil.com>
2021-02-14 20:23:30 +0530

    added header and improved the curent style

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
-const PORT = process.env.PORT || 5000; // you can use any port
-app.listen(PORT);
-console.log('server on ' + PORT);
